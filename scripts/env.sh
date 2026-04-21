127e20a452db379b881992fe174480d991a4b750
Rahul Javalagi <rahuljavalagi842@gmail.com>
2026-04-21 07:47:31 +0000

    Fix 33

--- scripts/env.sh
+++ scripts/env.sh
@@ -11,10 +11,10 @@ REGISTRY_IMAGE="${REGISTRY_IMAGE:-}"
 # Node addresses (ip:port)
 # Single laptop on Docker Desktop: host.docker.internal:<port>
 # Multi-laptop demo: replace with each laptop's real LAN IP:<port>
-NODE1_ADDR="${NODE1_ADDR:-100.119.215.61:50051}"
-NODE2_ADDR="${NODE2_ADDR:-100.93.179.62:50052}"
-NODE3_ADDR="${NODE3_ADDR:-100.121.43.25:50053}"
+NODE1_ADDR="${NODE1_ADDR:-100.78.101.127:50051}"
+NODE2_ADDR="${NODE2_ADDR:-100.116.88.75:50052}"
+NODE3_ADDR="${NODE3_ADDR:-100.119.37.82:50053}"
 
-#NODE1_ADDR="${NODE1_ADDR:-localhost:50051}"
-#NODE2_ADDR="${NODE2_ADDR:-localhost:50052}"
-#NODE3_ADDR="${NODE3_ADDR:-localhost:50053}"
+#NODE1_ADDR="${NODE1_ADDR:-host.docker.internal:50051}"
+#NODE2_ADDR="${NODE2_ADDR:-host.docker.internal:50052}"
+#NODE3_ADDR="${NODE3_ADDR:-host.docker.internal:50053}"
